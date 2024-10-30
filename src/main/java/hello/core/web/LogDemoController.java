d27fae29b408fdf4cc32a92c622bec4867f19c45
hjsong96 <ghkwlsdbwls@gmail.com>
2024-10-30 11:02:57 +0900

    bean scope finished

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,9 +3,14 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import static java.lang.Thread.sleep;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,13 +20,12 @@ public class LogDemoController {
 
     @RequestMapping("/log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        String requestURL = request.getRequestURI().toString();
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
-
-        myLogger.log("Controller");
+        myLogger.log("Controller test");
         logDemoService.logic("testId");
         return "OK";
     }
-
 }
